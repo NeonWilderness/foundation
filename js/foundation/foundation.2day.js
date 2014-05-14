5c36f2ccf2228a22fc8036cd8bba7f66fb3b32f4
NeonWilderness <neonwilderness@ymail.com>
2014-05-14 22:56:35 +0200

    Minor changes

--- js/foundation/foundation.2day.js
+++ js/foundation/foundation.2day.js
@@ -17,8 +17,8 @@
 //@prepros-prepend foundation.abide.js
 //@prepros-prepend foundation.js
 // Generate Foundation data-attributes out of Twoday-compatible HTML markup
-// Syntax: <elementtag title="data-someattr | data-text=sometext | data-class=someclass | data-id=someid">   will be transformed to
-//         <elementtag data-someattr data-text="sometext" data-class="someclass" data-id="someid">
+// Syntax: <elementtag title="data-someattr | data-text=sometext | data-class=someclass | data-id=someid | data-!placeholder=text">  will be transformed to
+//         <elementtag data-someattr data-text="sometext" data-class="someclass" data-id="someid" placeholder="text">
 ;(function($) { "use strict";
 
     $.fn.foundation2day = function(){
@@ -66,43 +66,48 @@
                     'data-tab':false,
                     'data-text':true,
                     'data-tooltip':false,
-                    'data-topbar':false
+                    'data-topbar':false,
+                    //special attributes
+                    'data-!placeholder':true, // holds the placeholder-text for input form fields
+                    'data-!title':true // holds the tooltip-text
                 },
                 init: function(useroptions){
-					//merge user options
+                    //merge user options
                     var options = $.extend( {}, foundation2dayObj.defaults, useroptions || {} ),
                         dataAttribs = [], parts = [], attrib = "", err = "Syntaxfehler: ", self;
-					//for each tag carrying a title with a "data-"-attr ...
+                    //for each tag carrying a title with a "data-"-attr ...
                     $foundationTags.each( function(){
-						//for the current element
+                        //for the current element
                         self = $(this);
-						//split multiple data-Attribs, if any
+                        //split multiple data-Attribs, if any
                         dataAttribs = (self.attr("title") || "").split("|");
-						//Now remove the title-Attrib
+                        //Now remove the title-Attrib
                         self.removeAttr("title");
-						//Process each data-Attrib
+                        //Process each data-Attrib
                         $.each( dataAttribs, function(){
-						//Isolate data-attrib and parameter literal, if any
+                        //Isolate data-attrib and parameter literal, if any
                             parts = $.trim(this).split("=");
                             attrib = parts[0].toLowerCase();
-							//Validate data-Attrib and existence of a potentially required parameter
+                            //Validate data-Attrib and existence of a potentially required parameter
                             if (options.validateDataAttribs){
                                 if (typeof foundation2dayObj.dataAttribs[attrib] === "undefined"){
-                                    console.log(err+attrib+'" ist kein bekanntes data-Attribut in Foundation5!');
+                                    console.error(err+attrib+' ist kein bekanntes data-Attribut in Foundation5!');
                                     return true;
                                 } else {
                                     if (foundation2dayObj.dataAttribs[attrib] && parts.length<2){
-                                        console.log(err+attrib+'" benötigt einen Parameter der Form '+attrib+'=param !');
+                                        console.error(err+attrib+' benötigt einen Parameter der Form '+attrib+'=param !');
                                         return true;
                                     }
                                 }
                             }
-							//Injects the extracted data-attrib into its HTML-tag
+                            //shapes special attributes (eliminates "data-!"-qualifier)
+                            attrib = (attrib.match(/data-!(.*)/) || ["",attrib])[1];
+                            //Injects the extracted data-attrib into its HTML-tag
                             self.attr(attrib, parts.length>1 ? parts[1] : "");
                         });
                     });
                     $(document).foundation(options.initFoundation5);
-					//Enable jQuery chaining
+                    //Enable jQuery chaining
                     return $foundationTags;
                 }
             };
